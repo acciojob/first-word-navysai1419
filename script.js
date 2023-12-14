0a36336f45590e1022065661478b75cd5f3b8be9
Navya <88023981+navysai1419@users.noreply.github.com>
2023-12-14 22:06:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstWord(s) {
+	const trimmedStr = str.trim();
+
+  // Find the index of the first space
+  const spaceIndex = trimmedStr.indexOf(' ');
+
+  // Return the first word or the entire string if no space is found
+  return spaceIndex !== -1 ? trimmedStr.slice(0, spaceIndex) : trimmedStr;
   // your code here
 }
 
-// Do not change the code below
+// // Do not change the code below 
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
